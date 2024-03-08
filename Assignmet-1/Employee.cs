9eb5a8be3e47a49cc9e92586a40988eec222ee99
ashish-kumar-mishra29 <ashishkumar.mishra110702@marwadiuniversity.ac.in>
2024-03-08 11:53:49 +0530

    First commit

---
+++ Assignmet-1/Employee.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignmet_1
+{
+    internal class Employee
+    {
+        public int Id;
+        public string Name;
+        public string DepartmentName;
+
+        public delegate void MethodCalledEventHandler(string methodName);
+
+        public event MethodCalledEventHandler MethodCalled;
+
+        public Employee(int id, string name, string departmentName)
+        {
+            Id = id;
+            Name = name;
+            DepartmentName = departmentName;
+        }
+
+        public int GetId()
+        {
+            MethodCalled.Invoke("GetId() method called");
+            return Id;
+        }
+
+        public string GetName()
+        {
+            MethodCalled.Invoke("GetName() method called");
+            return Name;
+        }
+
+        public string GetDepartmentName()
+        {
+            MethodCalled.Invoke("GetDepartmentName() method called");
+            return DepartmentName;
+        }
+
+        public void UpdateId(int id)
+        {
+            Id = id;
+        }
+
+        public void UpdateName(string name)
+        {
+            Name = name;
+        }
+
+        public void UpdateDepartmentName(string departmentName)
+        {
+            DepartmentName = departmentName;
+        }
+    }
+}
---
+++ Assignmet-1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Assignmet_1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+             Console.WriteLine("Enter Employee Id:");
+            int id = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Employee Name:");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Enter Employee Department Name:");
+            string departmentName = Console.ReadLine();
+
+            Employee employee = new Employee(id, name, departmentName);
+
+            // Subscribe to the MethodCalled event
+            employee.MethodCalled += (methodName) => Console.WriteLine(methodName);
+
+            // Printing properties using methods
+            Console.WriteLine("Id: " + employee.GetId());
+            Console.WriteLine("Name: " + employee.GetName());
+            Console.WriteLine("Department Name: " + employee.GetDepartmentName());
+
+            // Update properties
+            employee.UpdateId(1001);
+            employee.UpdateName("John Doe");
+            employee.UpdateDepartmentName("Engineering");
+
+            // Printing updated properties
+            Console.WriteLine("\nAfter Update:");
+            Console.WriteLine("Id: " + employee.GetId());
+            Console.WriteLine("Name: " + employee.GetName());
+            Console.WriteLine("Department Name: " + employee.GetDepartmentName());
+
+        }
+    }
+}
